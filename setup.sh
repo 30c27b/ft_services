4a1c220748e1d1a94f868d90ad9ae972067f8186
Antoine Coulon <antoine@30c27b.com>
2021-01-20 21:38:07 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,24 @@
 #    By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/20 14:12:58 by ancoulon          #+#    #+#              #
-#    Updated: 2021/01/20 14:12:59 by ancoulon         ###   ########.fr        #
+#    Updated: 2021/01/20 20:58:19 by ancoulon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+kubectl delete -f srcs/.
+minikube delete --all
+
+minikube start --driver=docker
+
+eval $(minikube docker-env)
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+docker build -t ft_services_nginx srcs/nginx/.
+
+kubectl create -f srcs/.
+
+minikube dashboard
