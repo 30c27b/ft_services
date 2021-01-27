667800cc37d25e09581541cdc373ac5888d380b4
Antoine Coulon <antoine@30c27b.com>
2021-01-27 12:06:40 +0100

    added wordpress container

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/20 14:12:58 by ancoulon          #+#    #+#              #
-#    Updated: 2021/01/26 15:13:33 by ancoulon         ###   ########.fr        #
+#    Updated: 2021/01/27 11:04:37 by ancoulon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,7 @@ eval $(minikube docker-env)
 
 docker build -t ft_services_mysql srcs/mysql/.
 docker build -t ft_services_phpmyadmin srcs/phpmyadmin/.
+docker build -t ft_services_wordpress srcs/wordpress/.
 docker build -t ft_services_nginx srcs/nginx/.
 
 
@@ -30,6 +31,7 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl apply -f srcs/metallb.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/nginx.yaml
 
 minikube dashboard
